888c38e2cec2137d1a9bf5ecc6a8e69302e5926e
kuyur <kuyur@kuyur.info>
2015-10-21 23:26:14 +0900

    add charmapmaker of tra2simp

---
+++ nextc4-charmapmaker-medium-chn-tra2simp/stdafx.cpp
@@ -0,0 +1,8 @@
+// stdafx.cpp : source file that includes just the standard includes
+// nextc4-charmapmaker-medium-chn-tra2simp.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
+
+// TODO: reference any additional headers you need in STDAFX.H
+// and not in this file
