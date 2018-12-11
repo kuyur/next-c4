9ca464d6ea45c2bcbc3815b1459b29b5c0dad90c
kuyur <kuyur@kuyur.info>
2018-12-11 11:17:16 +0900

    add gb18030 charmap maker project.

---
+++ nextc4-charmapmaker-back-u2gb18030/nextc4-charmapmaker-back-u2gb18030.cpp
@@ -0,0 +1,10 @@
+// nextc4-charmapmaker-back-u2gb18030.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+int main()
+{
+    return 0;
+}
+
---
+++ nextc4-charmapmaker-front-gb180302u/nextc4-charmapmaker-front-gb180302u.cpp
@@ -0,0 +1,11 @@
+// nextc4-charmapmaker-front-gb180302u.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int main()
+{
+    return 0;
+}
+
